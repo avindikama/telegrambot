5d37e9501dbc4b09a1645d7e0ec15812ec771348
Azamat Igiman <47282127+avindikama@users.noreply.github.com>
2022-01-16 02:51:26 -0500

    Update index.js
    added stickers

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ bot.on('message', msg => {
 })
 
 let bitcoinprice = 42.692;
-
+let senderName = "лох"
 const start = async () => {
 
 
@@ -62,11 +62,35 @@ const start = async () => {
 
                 })
                 console.log(getRandomInt(3));
+                senderName = msg.from.first_name
+                const cars = ["Иди нахуй "+String(senderName), "Чина лох", "Скиньте бабки на Каспи 5169 4971 4978 0037, Я айфон хочу", "Ля ты конченный", "Я не агрессор"];
+                cars.push('Э, подлиза, че делаешь? ')
+                cars.push('Ну ты и пидорррррас ' )
+                cars.push('Пидорррррасул' )
+                cars.push('Ляяя ты крыса ' )
+                cars.push('Ну ты и конч ' )
+                cars.push('Ну ладно пошла я нахуй                    Deenah покинула чат ')
+                cars.push('Азамат такой охуенный')
+                cars.push('Рукаблуд')
 
-                const cars = ["Иди нахуй", "Чина лох", "Скиньте бабки на Каспи 5169 4971 4978 0037, Я айфон хочу", "Ля ты конченный", "Я не агрессор"];
-                cars.push('Даур блять Курс биткоина ' + String(bitcoinprice))
-                await bot.sendSticker(chatId, 'https://tlgrm.ru/_/stickers/b50/063/b5006369-8faa-44d7-9f02-1ca97d82cd49/9.webp')
-                return bot.sendMessage(chatId, cars[getRandomInt(6)]);
+
+                let stickers = ['https://tlgrm.ru/_/stickers/b50/063/b5006369-8faa-44d7-9f02-1ca97d82cd49/9.webp',
+                'https://tlgrm.eu/_/stickers/ccd/a8d/ccda8d5d-d492-4393-8bb7-e33f77c24907/1.webp' ,
+                    'https://tlgrm.eu/_/stickers/325/d8b/325d8b30-558a-49ed-b6e6-cf4f01e63c11/1.webp',
+                    'https://tlgrm.eu/_/stickers/325/d8b/325d8b30-558a-49ed-b6e6-cf4f01e63c11/2.webp',
+                    'https://tlgrm.eu/_/stickers/325/d8b/325d8b30-558a-49ed-b6e6-cf4f01e63c11/3.webp',
+                    'https://tlgrm.eu/_/stickers/325/d8b/325d8b30-558a-49ed-b6e6-cf4f01e63c11/4.webp',
+                    'https://tlgrm.eu/_/stickers/325/d8b/325d8b30-558a-49ed-b6e6-cf4f01e63c11/5.webp',
+                    'https://tlgrm.eu/_/stickers/325/d8b/325d8b30-558a-49ed-b6e6-cf4f01e63c11/7.webp',
+                    'https://tlgrm.eu/_/stickers/325/d8b/325d8b30-558a-49ed-b6e6-cf4f01e63c11/8.webp',
+                    'https://tlgrm.eu/_/stickers/325/d8b/325d8b30-558a-49ed-b6e6-cf4f01e63c11/9.webp',
+                    'https://tlgrm.eu/_/stickers/325/d8b/325d8b30-558a-49ed-b6e6-cf4f01e63c11/10.webp',
+                    'https://tlgrm.eu/_/stickers/325/d8b/325d8b30-558a-49ed-b6e6-cf4f01e63c11/11.webp',
+                    'https://tlgrm.eu/_/stickers/325/d8b/325d8b30-558a-49ed-b6e6-cf4f01e63c11/12.webp'
+                ]
+
+                await bot.sendSticker(chatId, stickers[getRandomInt(stickers.length)] )
+                return bot.sendMessage(chatId, cars[getRandomInt(cars.length)]);
             }
 
         })
